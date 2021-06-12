1d09f84cf089582a757c36df64218af349c0f9ca
12Aleks <tanais2010@yahoo.com>
2021-06-12 10:13:47 +0200

    display data labels

--- app/src/components/future.js
+++ app/src/components/future.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState, useRef} from 'react';
 import {Line} from "react-chartjs-2";
-
-
-
+import {Chart} from 'chart.js';
+import ChartDataLabels from "chartjs-plugin-datalabels";
+Chart.register(ChartDataLabels);
 
 const Future = ({selected}) => {
     const [data, setData] = useState(null);
@@ -10,8 +10,7 @@ const Future = ({selected}) => {
 
 
 
-
-    const chart = (selected) => {
+    const chart = (selected, gradient) => {
         setData({
             labels: ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'],
             datasets: [
@@ -19,9 +18,9 @@ const Future = ({selected}) => {
                     label: '# of Votes',
                     data: selected,
                     fill: true,
-                    scaleBeginAtZero : true,
-                    radius: 4,
-                    backgroundColor: 'rgba(26,70,183, 0.2)' ,
+                    scaleBeginAtZero: true,
+                    radius: 2,
+                    backgroundColor: gradient,
                     borderColor: 'rgba(26,70,183, 0.2)',
                 },
             ]
@@ -29,46 +28,70 @@ const Future = ({selected}) => {
 
         setOptions({
             animation: false,
-            plugins: {
-                legend: {
-                    display: false
-                },
-            },
             elements: {
                 line: {
                     tension: .4
                 }
             },
             scales: {
-                xAxes: [{
-                    display: false
-                }],
-                yAxes: {
-                    ticks: {
-                        beginAtZero: true,
+                yAxes: [{
+                    gridLines: {
+                        drawOnChartArea: false,
+                        drawBorder: false,
+                        tickMarkLength: 0,
                     },
-
-                }
+                    ticks: {
+                        suggestedMin: 13, // lowest from data minus 2/3
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        drawOnChartArea: false,
+                        drawBorder: false,
+                        tickMarkLength: 0,
+                    },
+                    ticks: {
+                        fontColor: '#000000',
+                    }
+                }]
             },
-
             layout: {
                 padding: {
                     top: 10,
                     bottom: 10
                 }
-            }
+            },
+
+            plugins: {
+                legend: {
+                    display: false
+                },
+                datalabels: {
+                    align: 'top',
+                    color: '#000',
+                    font: {
+                        weight: 'bold'
+                    },
+                    display: true,
+                },
+            },
         })
     };
 
 
-
     useEffect(() => {
-        chart(selected.rez)
+        let ctx = document.getElementById('canvas').getContext("2d")
+        let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+        gradient.addColorStop(0, 'rgba(251,189,8, .6)');
+        gradient.addColorStop(.6, 'rgba(26,70,183,.6)');
+        gradient.addColorStop(1, 'rgba(26,70,183, .8)');
+        chart(selected.rez, gradient)
     }, [selected]);
 
     return (
         <div className='futureDay pt-2 pb-2'>
-            <Line data={data} options={options}/>
+            <Line id='canvas' data={data}  options={options}/>
         </div>
     );
 };
